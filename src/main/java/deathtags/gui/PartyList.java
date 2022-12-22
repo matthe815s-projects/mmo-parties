e885956e5882cc7904d5707a83063acc3d5b8dda
Matthe815 <matthew.elswick@matthe815.dev>
2022-12-22 16:42:25 -0500

    bugfix: Fix error in naming

--- src/main/java/deathtags/gui/PartyList.java
+++ src/main/java/deathtags/gui/PartyList.java
@@ -276,7 +276,7 @@ public class PartyList {
             case "percentage": // This will show the x% after bars.
                 Minecraft.getInstance().font.drawShadow(stack, String.format("%s",Math.floor(extraHearts / max * 100)) + "%", remainderX, UI.y, 0xFFFFFF);
                 break;
-            case "relative": // This will show the x/x after the bars.
+            case "compare": // This will show the x/x after the bars.
                 Minecraft.getInstance().font.drawShadow(stack, String.format("%s/%s",extraHearts, max), remainderX, UI.y, 0xFFFFFF);
                 break;
             case "additional": // This will show +X after health.
