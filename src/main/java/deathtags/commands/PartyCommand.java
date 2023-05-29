4d5f7876b7302e0172bdc04a7eb76918bc15223c
Matthe815 <matthew.elswick@matthe815.dev>
2023-05-28 22:13:05 -0400

    fix: Fixed wrong values

--- src/main/java/deathtags/commands/PartyCommand.java
+++ src/main/java/deathtags/commands/PartyCommand.java
@@ -113,7 +113,7 @@ public class PartyCommand {
 				break;
 
 			case "invite":
-				if (targetStr == null) { CommandMessageHelper.SendError(player, "rpgparties.message.error.argument", player.getName().getContents()); return 0; }
+				if (targetStr == null) { CommandMessageHelper.SendError(player, "rpgparties.message.error.argument", player.getName().getString()); return 0; }
 				if (!stats.InParty()) Party.Create ( player ); // Create a party to invite with if not existent.
 				
 				stats.party.Invite ( player, target ); // Send an invite to the target player.
